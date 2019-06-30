1fa571e4242a708f27be8c0a7092272b133e59d3
lyy <lyyang33@163.com>
2019-07-01 00:00:03 +0800

    logging error handled

--- dailyUpdate.py
+++ dailyUpdate.py
@@ -7,10 +7,13 @@ import logging
 if __name__ == "__main__":
 
 
-    logging.basicConfig(filename="dailyUpdate.log", filemode="w", format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
-                        datefmt="%d-%M-%Y %H:%M:%S", level=logging.INFO)
+
 
     while True:
+
+        logging.basicConfig(filename="dailyUpdate.log", filemode="w",
+                            format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
+                            datefmt="%d-%M-%Y %H:%M:%S", level=logging.INFO)
 
         logging.info("Daily Update Start ————")
 
@@ -23,5 +26,6 @@ if __name__ == "__main__":
         dailyUpdateByCcpe(connection)
        #dailyUpdateByBooked()
         logging.info("Daily Update Finished.")
+        print(time.asctime(time.localtime(time.time()))+"Daily Update Finished")
         connection.close()
-        time.sleep(60*60*24)
+        #time.sleep(60*60*24)
